8fc4f0bae60988b212ba51b657abb69457632e26
farzia <ghaforyfarzia@gmail.com>
2026-01-26 14:16:30 +0100

    Added NotFoundException class

--- src/main/java/es/fplumara/dam1/actividades/exception/NotFoundException.java
+++ src/main/java/es/fplumara/dam1/actividades/exception/NotFoundException.java
@@ -1,4 +1,10 @@
 package es.fplumara.dam1.actividades.exception;
 
-public class NotFoundException {
+public class NotFoundException extends RuntimeException {
+   public NotFoundException() {
+       super();
+   }
+   public NotFoundException(String message){
+       super(message);
+   }
 }
