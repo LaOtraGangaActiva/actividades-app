fec0e75cffec4cab5163603b5b8484f65facf2b5
farzia <ghaforyfarzia@gmail.com>
2026-01-27 11:42:05 +0100

    created UsuarioRepository , TallerRepository

--- src/main/java/es/fplumara/dam1/actividades/repository/UsuarioRepository.java
+++ src/main/java/es/fplumara/dam1/actividades/repository/UsuarioRepository.java
@@ -1,4 +1,13 @@
 package es.fplumara.dam1.actividades.repository;
 
-public class UsuarioRepository {
+import es.fplumara.dam1.actividades.model.Usuario;
+import java.util.List;
+import java.util.Optional;
+
+public interface UsuarioRepository {
+    Usuario save(Usuario usuario);
+    Optional<Usuario> findById(long id);
+    List<Usuario> findByEmail(String email);
+    Optional<Usuario> findByDiscordUserId(String discordUserId);
+    void deleteById(Long id);
 }
