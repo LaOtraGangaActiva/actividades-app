f2accac05d67caa674f7bb918d4b78945fca1e35
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2026-01-28 12:47:58 +0100

    update UsuarioService

--- src/main/java/es/fplumara/dam1/actividades/service/UsuarioService.java
+++ src/main/java/es/fplumara/dam1/actividades/service/UsuarioService.java
@@ -7,33 +7,9 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface UsuarioService {
-Usuario crearUsuario(Usuario usuario);
-List<Usuario> listarUsuario();
-Optional<Usuario> obtenerUsuario(UUID id);
-Usuario actualizarUsuario(Long id, Usuario usuario);
-void eliminarUsuario(UUID id);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*package es.fplumara.dam1.actividades.service;
-
-
-
-
+    Usuario crearUsuario(Usuario usuario);
+    List<Usuario> listarUsuario();
+    Optional<Usuario> obtenerUsuario(UUID id);
+    Usuario actualizarUsuario(Long id, Usuario usuario);
+    void eliminarUsuario(UUID id);
 }
