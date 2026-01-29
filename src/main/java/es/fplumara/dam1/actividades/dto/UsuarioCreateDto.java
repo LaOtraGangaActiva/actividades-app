0be2f3896f1e3890ff14421356dcf49891fa9067
farzia <ghaforyfarzia@gmail.com>
2026-01-29 08:52:13 +0100

    crerate DTOs de usuario

--- src/main/java/es/fplumara/dam1/actividades/dto/UsuarioCreateDto.java
+++ src/main/java/es/fplumara/dam1/actividades/dto/UsuarioCreateDto.java
@@ -1,4 +1,35 @@
 package es.fplumara.dam1.actividades.dto;
 
+import es.fplumara.dam1.actividades.model.PerfilUsuario;
+
 public class UsuarioCreateDto {
+    private String nombre;
+    private PerfilUsuario perfil;
+    private String discordUserId;
+    private String curso;
+    private String email;
+    public UsuarioCreateDto(String nombre, PerfilUsuario perfil, String discordUserId, String curso, String email) {
+        this.nombre = nombre;
+this.perfil=perfil;
+this.discordUserId=discordUserId;
+this.curso=curso;
+this.email=email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public PerfilUsuario getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(PerfilUsuario perfil) {
+        this.perfil = perfil;
+    }
+
 }
