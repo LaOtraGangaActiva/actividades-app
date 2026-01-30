6658db2d3da4a51665b5b520087a6cbf7ab748c9
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2026-01-30 14:15:38 +0100

    implementación falta terminar jakarta y tests

--- src/main/java/es/fplumara/dam1/actividades/dto/TallerCreateDto.java
+++ src/main/java/es/fplumara/dam1/actividades/dto/TallerCreateDto.java
@@ -2,20 +2,28 @@ package es.fplumara.dam1.actividades.dto;
 
 import es.fplumara.dam1.actividades.model.EstadoInscripcion;
 import es.fplumara.dam1.actividades.model.Taller;
+import jakarta.validation.constraints.*;
 
 public record TallerCreateDto(
 
+        @NotBlank
+        @Size(max=25)
         String titulo,
 
+        @NotBlank
         String descripcion,
 
+        @NotNull
         EstadoInscripcion estadoInscripcion,
 
+        @NotBlank
         String url,
 
+        @PositiveOrZero
+        @Max(100)
         int cupo,
 
+        @NotBlank
         String lugar
 ) {
-
 }
