0e5fd14f3260a48322840ab9ec896aef4b5c50cc
farzia <ghaforyfarzia@gmail.com>
2026-02-04 13:33:36 +0100

    Mild changes

--- src/main/java/es/fplumara/dam1/actividades/config/AppFactory.java
+++ src/main/java/es/fplumara/dam1/actividades/config/AppFactory.java
@@ -1,4 +1,18 @@
-package es.fplumara.dam1.actividades.config;
+/*package es.fplumara.dam1.actividades.config;
+
+import es.fplumara.dam1.actividades.service.InscripcionService;
+import es.fplumara.dam1.actividades.service.TallerService;
+import es.fplumara.dam1.actividades.service.UsuarioService;
 
 public class AppFactory {
-}
+
+        public static AppContext createAppContext() {
+            // aquí montamos repos in-memory + service.impl como ya hemos hecho
+            UsuarioService usuarioService = ...;
+            TallerService tallerService = ...;
+            InscripcionService inscripcionService = ...;
+
+            return new AppContext(usuarioService, tallerService, inscripcionService);
+        }
+    }*/
+
---
+++ src/main/java/es/fplumara/dam1/actividades/discord/DiscordBotMain.java
@@ -0,0 +1,36 @@
+package es.fplumara.dam1.actividades.discord;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+
+public class DiscordBotMain {
+
+    // Pon aquí el ID del servidor de pruebas (Guild ID)
+    private static final String GUILD_ID = "1468562633304051886";
+
+    public static void main(String[] args) throws Exception {
+        String token = System.getenv("DISCORD_TOKEN");
+        if (token == null || token.isBlank()) {
+            throw new IllegalStateException("Falta DISCORD_TOKEN en variables de entorno");
+        }
+
+        // 2) Arrancamos JDA
+        JDA jda = JDABuilder.createDefault(token)
+                .addEventListeners(new DiscrodCommands.DiscordCommands())
+                .build()
+                .awaitReady();
+
+        // 3) Registramos slash commands
+        var guild = jda.getGuildById(GUILD_ID);
+        if (guild == null) {
+            throw new IllegalStateException("No encuentro el guild con id " + GUILD_ID);
+        }
+        guild.upsertCommand("ping","Pasa la bola!").queue();
+        guild.upsertCommand("hora","Dime la hora")
+                .addOption(OptionType.STRING, "zone", "cualquir Lugar del mundo")
+                .queue();
+
+
+    }
+}
+
