fb308512b02731cc7d887541d3c375f6bb35b997
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2026-02-06 12:49:39 +0100

    app factory)

--- src/main/java/es/fplumara/dam1/actividades/config/AppFactory.java
+++ src/main/java/es/fplumara/dam1/actividades/config/AppFactory.java
@@ -1,4 +1,16 @@
 package es.fplumara.dam1.actividades.config;
 
+import es.fplumara.dam1.actividades.service.*;
+import es.fplumara.dam1.actividades.service.impl.UsuarioServiceImpl;
+
 public class AppFactory {
+
+    public static AppContext createAppContext() {
+        // aquí montamos repos in-memory + service.impl como ya hemos hecho
+        UsuarioService usuarioService = new UsuarioServiceImpl();
+        TallerService tallerService ;
+        InscripcionService inscripcionService ;
+
+        return new AppContext(usuarioService, tallerService, inscripcionService);
+    }
 }
